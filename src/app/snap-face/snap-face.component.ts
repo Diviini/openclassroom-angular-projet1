456ae8a52dc86551a31aecd96fa5f6adb421a4a0
Divini <92796539+Diviini@users.noreply.github.com>
2023-09-21 17:08:21 +0200

    Poursuite du cours
    - Construction du component
    - Affichage dynamique
    - Event

--- src/app/snap-face/snap-face.component.ts
+++ src/app/snap-face/snap-face.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+// Quand il y a un @ c'est un "décorateur", cela apporte des modifications à une classe
 @Component({
   selector: 'app-snap-face',
-  templateUrl: './snap-face.component.html',
+  templateUrl: './snap-face.component.html', 
   styleUrls: ['./snap-face.component.scss']
 })
-export class SnapFaceComponent {
+
+// Création de la classe
+export class SnapFaceComponent implements OnInit {
+
+  // Déclaration des variables
+  titre!: string;
+  description!: string;
+  date!: Date;
+  like!: number;
+  imageUrl!: string;
+  haveLiked!: boolean;
+  titreLike!: string;
+
+  /* La méthode  ngOnInit()  est appelée automatiquement par Angular au moment de la création de chaque instance du component. 
+  Elle permet notamment d'initialiser des propriétés. */
+  ngOnInit(): void {
+      this.titre = "Mon Titre";
+      this.description = "Voici une belle description !";
+      this.date = new Date();
+      this.like = 5;
+      this.imageUrl = "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg";
+      this.haveLiked = false;
+      this.titreLike = "Like";
+  }
+
+  // Le type d'une fonction est facultative
+  onLike()/*:number*/ {
+    if ( this.haveLiked == false) {
+      this.haveLiked = true;
+      this.titreLike = "Dislike";
+      this.like++;
+      
+    } else {
+      this.haveLiked = false;
+      this.titreLike = "Like";
+      this.like--;
+      
+    }
+
+  }
 
 }
