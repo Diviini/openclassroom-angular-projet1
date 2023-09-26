4fa1da96a7df935ccb5b99fb4dd7ff405c6487ed
Divini <92796539+Diviini@users.noreply.github.com>
2023-09-26 15:16:34 +0200

    Ajout des if et for ainsi que d'une liste de snapFace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,40 @@ import { SnapFace } from './models/snap-face.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  snapFaces!: SnapFace[];
+
+  /*
   monSnap!:     SnapFace;
   autreSnap!:   SnapFace;
   dernierSnap!: SnapFace;
-
+  */
 
   ngOnInit(): void {
-    this.monSnap = new SnapFace(
-      "Mon Titre",
-      "Voici une belle description !", 
-      "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg", 
-      new Date(), 
-      0
-    );
-    this.autreSnap = new SnapFace(
-      "2e Titre",
-      "Voici une simple description !", 
-      "https://cdn.pixabay.com/photo/2023/09/07/14/26/cat-8239223_1280.png", 
-      new Date(), 
-      2
-    );
-    this.dernierSnap = new SnapFace(
-      "Mon 3e titre",
-      "babaji !", 
-      "https://cdn.pixabay.com/photo/2023/06/28/10/00/ai-generated-8093991_640.jpg", 
-      new Date(), 
-      0
-    );
+
+    this.snapFaces = [
+    {
+      titre: "Mon Titre",
+      description: "Voici une belle description !", 
+      imageUrl: "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg", 
+      date: new Date(), 
+      like: 0
+    },
+    {
+      titre: "2e Titre",
+      description: "Voici une simple description !", 
+      imageUrl: "https://cdn.pixabay.com/photo/2023/09/07/14/26/cat-8239223_1280.png", 
+      date: new Date(), 
+      like: 2
+    },
+    {
+      titre: "Mon 3e titre",
+      description: "babaji !", 
+      imageUrl: "https://cdn.pixabay.com/photo/2023/06/28/10/00/ai-generated-8093991_640.jpg", 
+      date: new Date(), 
+      like: 0,
+      location: "Lille"
+    }];
+   
   }
 }
--- src/app/models/snap-face.model.ts
+++ src/app/models/snap-face.model.ts
@@ -1,13 +1,25 @@
 export class SnapFace {
+    titre!: string;
+    description!: string;
+    imageUrl!: string;
+    date!: Date;
+    like!: number;
+    location?: string;
+}
+
+/* Méthode plus compact mais lourde
+export class SnapFace {
     constructor(
         public titre:string,
         public description: string,
         public imageUrl: string,
         public date: Date,
-        public like: number
+        public like: number,
+        public location?: string //Propriété optionnelle quand '?'
     ) {}
     
 }
+*/
 
 /* Méthode classique
 export class SnapFace {
