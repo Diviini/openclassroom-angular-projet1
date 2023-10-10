3f8dcca9397c3326a6d3f359611c9b45e8adf55d
diviini <robin.lebrozec@gmail.com>
2023-10-10 14:31:07 +0200

    Fin du cours sur les observable bas niveau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Observable, interval, map, filter, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
-  interval$!: Observable<number>;
+  interval$!: Observable<string>;
 
   ngOnInit(): void {
+    this.interval$ = interval(1000).pipe(
+      //Filtre la sortie
+      filter((value) => value % 3 === 0),
 
-    this.interval$ = interval(1000);
+      //Change les valeurs
+      map((value) =>
+        value % 2 === 0
+          ? `Je suis ${value} et je suis pair`
+          : `Je suis ${value} et je suis impair`
+      ),
 
+      //Modifie sans changer
+      tap((text) => this.logger(text))
+    );
+  }
+
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
   }
 }
--- src/app/single-snapface/single-snapface.component.ts
+++ src/app/single-snapface/single-snapface.component.ts
@@ -6,40 +6,37 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-single-snapface',
   templateUrl: './single-snapface.component.html',
-  styleUrls: ['./single-snapface.component.scss']
+  styleUrls: ['./single-snapface.component.scss'],
 })
 export class SingleSnapfaceComponent implements OnInit {
-
-  @Input() snapface!: SnapFace
+  @Input() snapface!: SnapFace;
 
   haveLiked!: boolean;
   titreLike!: string;
 
-  constructor(private snapFaceServices: SnapFaceService,
-              private route: ActivatedRoute ) { }
+  constructor(
+    private snapFaceServices: SnapFaceService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-
     this.haveLiked = false;
-    this.titreLike = "Like";
+    this.titreLike = 'Like';
 
     //Ajout du + = Integer.parseInt
     const snapFaceId = +this.route.snapshot.params['id'];
     this.snapface = this.snapFaceServices.getSnapFaceById(snapFaceId);
   }
 
-  onLike():void {
+  onLike(): void {
     if (this.haveLiked == false) {
       this.haveLiked = true;
-      this.titreLike = "Dislike";
+      this.titreLike = 'Dislike';
       this.snapFaceServices.snapSnapFaceById(this.snapface.id, this.haveLiked);
-
     } else {
       this.haveLiked = false;
-      this.titreLike = "Like";
+      this.titreLike = 'Like';
       this.snapFaceServices.snapSnapFaceById(this.snapface.id, this.haveLiked);
-
     }
-
   }
 }
