59cc82891259d6bbc123a5e6131697e84f9f29c9
Divini <92796539+Diviini@users.noreply.github.com>
2023-10-10 16:21:35 +0200

    Changement de machine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval, map, filter, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
+  interval$!: Observable<string>;
 
   ngOnInit(): void {
+    this.interval$ = interval(1000).pipe(
+      //Filtre la sortie
+      filter((value) => value % 3 === 0),
 
+      //Change les valeurs
+      map((value) =>
+        value % 2 === 0
+          ? `Je suis ${value} et je suis pair`
+          : `Je suis ${value} et je suis impair`
+      ),
+
+      //Modifie sans changer
+      tap((text) => this.logger(text))
+    );
+  }
+
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
   }
 }
