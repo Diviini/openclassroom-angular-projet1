3cebc83c64b1606b701af4631412a4b707314b2b
diviini <robin.lebrozec@gmail.com>
2023-10-10 13:40:36 +0200

    Commencement du 2e cours :
    - Ajout d'Observale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  interval$!: Observable<number>;
 
   ngOnInit(): void {
+
+    this.interval$ = interval(1000);
 
   }
 }
