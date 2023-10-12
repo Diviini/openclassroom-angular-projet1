ec3400e0b2ca437fbf0879149d65ceda7491cb32
diviini <robin.lebrozec@gmail.com>
2023-10-12 11:31:36 +0200

    Début de création d'un formulaire réactif

--- src/app/new-snap-face/new-snap-face.component.ts
+++ src/app/new-snap-face/new-snap-face.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-snap-face',
   templateUrl: './new-snap-face.component.html',
   styleUrls: ['./new-snap-face.component.scss']
 })
-export class NewSnapFaceComponent {
+export class NewSnapFaceComponent implements OnInit {
+  
+  snapForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder)  {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
