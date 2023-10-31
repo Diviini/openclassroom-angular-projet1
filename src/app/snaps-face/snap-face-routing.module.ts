5a497b21b40a86a2d97999b66b33a585e8e79b2b
Diviini <92796539+Diviini@users.noreply.github.com>
2023-10-31 12:31:05 +0100

    Mise en place d'un gard

--- src/app/snaps-face/snap-face-routing.module.ts
+++ src/app/snaps-face/snap-face-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { SnapFaceListComponent } from "./components/snap-face-list/snap-face-list.component";
 import { NewSnapFaceComponent } from "./components/new-snap-face/new-snap-face.component";
 import { SingleSnapfaceComponent } from "./components/single-snapface/single-snapface.component";
+import { AuthGuard } from "../core/guards/auth.gard";
 
 const routes: Routes = [
     
-    { path: 'create'  , component: NewSnapFaceComponent   },
-    { path: ':id'     , component: SingleSnapfaceComponent},
-    { path: ''        , component: SnapFaceListComponent  },
+    { path: 'create'  , component: NewSnapFaceComponent   , canActivate: [AuthGuard]},
+    { path: ':id'     , component: SingleSnapfaceComponent, canActivate: [AuthGuard]},
+    { path: ''        , component: SnapFaceListComponent  , canActivate: [AuthGuard]},
 ]
 
 @NgModule({
